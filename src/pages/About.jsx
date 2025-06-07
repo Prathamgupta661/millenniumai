e8046a3c021cae1a3c4dfc695d5abd9f298533a7
Pratham Gupta <guptapratham661@gmail.com>
2025-06-07 13:16:08 +0530

    Add About page content and image; enhance Home page layout and sections

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,11 +1,59 @@
-import React from 'react'
+import img1 from "../assets/about.webp";
 
 const About = () => {
   return (
-    <div>
-      About Page
-    </div>
-  )
-}
+    <section className="mx-5 ">
+      <div className="my-6">
+        <div className="sm:flex  gap-10 items-center">
+          <div className="w-full flex flex-col items-center justify-center m-2">
+            <h2 className=" text-center w-[70%] text-4xl leading-15 font-bold font-dm">
+              Where Artificial Intelligence Meets Full-Stack Innovation
+            </h2>
+            <button className="cursor-pointer px-5 py-2 bg-gradient-to-r from-cyan-500 to-pink-500 font-dm rounded-md h-11 hover:border border-black">
+              Contact Us
+            </button>
+          </div>
+          <div className="w-full m-2 flex items-center justify-center">
+            
+          <img src={img1} alt="img" className="sm:w-95 w-80 h-80 sm:h-70 rounded-2xl" />
+          </div>
+        </div>
+              <h1 className="my-5 text-center font-dm text-2xl font-bold">About Us</h1>
 
-export default About
+
+        <p className="text-lg text-gray-500 w-[90%] mx-auto ">
+          MillenniumAi started with a singular vision — to power AI systems with
+          high-quality, human-labeled data. Today, we’ve evolved into a
+          multi-disciplinary technology powerhouse delivering end-to-end
+          solutions that span AI Data Annotation, Web Development, Mobile &
+          Gaming App Development, SEO, and Full Stack Software Engineering.
+        </p>
+
+        <p className="text-lg text-gray-500 w-[90%] mx-auto mt-5">
+          With a diverse team of engineers, annotators, developers, designers,
+          and strategists, we’ve successfully served clients in eCommerce,
+          EdTech, Fitness, Healthcare, Logistics, and more.
+        </p>
+        <ul className="text-lg text-gray-500 list-disc w-[90%] mx-auto mt-5">
+          <p>We take pride in being:</p>
+          <li className="ml-10">
+            A trusted partner to over 25 growing and established companies
+          </li>
+          <li className="ml-10">
+            {" "}
+            One of the fastest-growing digital transformation companies in India
+          </li>
+          <li className="ml-10"> A seamless blend of AI intelligence and human creativity</li>
+        </ul>
+
+        <p className="text-lg text-gray-500 w-[90%] mx-auto mt-5">
+          At MillenniumAi, innovation isn’t just a buzzword — it’s the
+          foundation of how we deliver excellence and build long-term
+          partnerships.
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default About;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,54 @@
-
 const Home = () => {
   return (
-    <section className="w-full h-[93vh] overflow-hidden flex flex-col items-center justify-center">
-      <h1 className="text-center w-[75%] sm:w-[60%] mx-auto text-5xl sm:text-8xl font-medium font-dm">
-        Accelerate Your <span className="text-transparent bg-clip-text bg-gradient-to-r from-cyan-500 to-pink-500">AI</span> with Precision Annotation
-      </h1>
-      <p className="m-5 text-center text-slate-500 text-lg sm:text-xl">Image, Video & Geospatial Annotation Services powered by experts and smart tech.</p>
-      <button className="cursor-pointer px-5 py-2 bg-gradient-to-r from-cyan-500 to-pink-500 font-dm rounded-md h-11 mx-auto hover:border border-black">Get Started</button>
+    <section className="w-full min-h-full overflow-hidden flex flex-col">
+      <div className="flex flex-col items-center justify-center mt-32">
+        <h1 className="text-center w-[75%] sm:w-[60%] mx-auto text-5xl sm:text-8xl font-medium font-dm">
+          Accelerate Your{" "}
+          <span className="text-transparent bg-clip-text bg-gradient-to-r from-cyan-500 to-pink-500">
+            AI
+          </span>{" "}
+          with Precision Annotation
+        </h1>
+        <p className="m-5 text-center text-slate-500 text-lg sm:text-xl">
+          Image, Video & Geospatial Annotation Services powered by experts and
+          smart tech.
+        </p>
+        <button className="cursor-pointer px-5 py-2 bg-gradient-to-r from-cyan-500 to-pink-500 font-dm rounded-md h-11 mx-auto hover:border border-black">
+          Get Started
+        </button>
+      </div>
+      <section className="mt-40 mx-auto m-5 w-[95%] sm:flex shadow-xl border border-transparent rounded-lg bg-clip-border bg-gradient-to-r from-cyan-500 to bg-pink-500">
+        <div className="w-full p-5 bg-white">
+          <h2 className="font-dm text-xl font-bold">
+            Empowering Innovation with AI & Full-Stack Development
+          </h2>
+          <p className="mt-3 w-[90%] text-slate-500 ">
+            {" "}
+            MillenniumAi is one of the fastest-growing technology partners,
+            trusted by startups and enterprises alike. From intelligent data
+            annotation for AI models to cutting-edge website, mobile, and gaming
+            app development — we offer a complete suite of digital services that
+            transform ideas into reality.
+          </p>
+          <p className="mt-4 text-slate-500 w-[90%]">
+            {" "}
+            Our dual strength in artificial intelligence and software
+            development enables us to deliver smart, scalable, and secure
+            solutions. With a proven track record across industries, we are the
+            preferred partner for organizations looking to accelerate digital
+            transformation with speed and precision.
+          </p>
+
+          <button className="mt-5 cursor-pointer">
+            <span className="text-transparent bg-clip-text bg-gradient-to-r from-cyan-500 to-pink-500 hover:text-pink-500">
+              → Let’s Build the Future Together.
+            </span>
+          </button>
+        </div>
+        <div className="w-full flex items-center justify-center bg-white">
+          <h1>img</h1>
+        </div>
+      </section>
     </section>
   );
 };
